29cbd1cdca18f3bb89de7411957d7fe41cd051b2
somnambulistic-art <somnambulistic.traveller@gmail.com>
2021-06-04 12:47:13 +0300

    add celery task "send_email" to signals.py

--- api_diplom_final/celery.py
+++ api_diplom_final/celery.py
@@ -1,16 +1,21 @@
 import os
+from api_diplom_final.settings import EMAIL_HOST_USER
+from django.core.mail.message import EmailMultiAlternatives
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_django.settings')
 app = Celery('api_diplom_final')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# загрузка tasks.py в приложение django
 app.autodiscover_tasks()
 
 
-
-@app.task
-def send_email(some_data):
-    return
-# TODO:добавить задачу с отправкой почты
+@app.task()
+def send_email(title, message: str, email: str):
+    email_list = list()
+    email_list.append(email)
+    try:
+        message = EmailMultiAlternatives(subject=title, body=message, from_email=EMAIL_HOST_USER, to=email_list)
+        message.send()
+        return f'Title: {message.subject}, Message:{message.body}'
+    except Exception:
+        raise Exception
--- usermanager/signals.py
+++ usermanager/signals.py
@@ -1,9 +1,9 @@
-from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
 from django.dispatch import receiver, Signal
 from django_rest_passwordreset.signals import reset_password_token_created
 
 from usermanager.models import ConfirmEmailToken, User
+from api_diplom_final.celery import send_email
+
 
 new_user_registered = Signal(
     providing_args=['user_id'],
@@ -22,40 +22,22 @@ def password_reset_token_created(sender, instance, reset_password_token, **kwarg
     :param sender: View Class that sent the signal
     :param instance: View Instance that sent the signal
     :param reset_password_token: Token Model Object
-    :param kwargs:
-    :return:
     """
 
-    msg = EmailMultiAlternatives(
-        # title:
-        f"Password Reset Token for {reset_password_token.user}",
-        # message:
-        reset_password_token.key,
-        # from:
-        settings.EMAIL_HOST_USER,
-        # to:
-        [reset_password_token.user.email]
-    )
-    msg.send()
+    title = f'Сброс пароля пользователя: {reset_password_token.user}'
+    message = f'Токен: {reset_password_token.key}'
+    email = reset_password_token.user
+    send_email(title, message, email)
 
 
 @receiver(new_user_registered)
 def new_user_registered_signal(user_id, **kwargs):
     """ Отправка письмо с подтверждением почты. """
-    # send an e-mail to the user
     token, _ = ConfirmEmailToken.objects.get_or_create(user_id=user_id)
-
-    msg = EmailMultiAlternatives(
-        # title:
-        f"Password Reset Token for {token.user.email}",
-        # message:
-        token.key,
-        # from:
-        settings.EMAIL_HOST_USER,
-        # to:
-        [token.user.email]
-    )
-    msg.send()
+    title = f'Подтверждение регистрации пользователя: {token.user.email}'
+    message = f'Токен: {token.key}'
+    email = token.user.email
+    send_email(title, message, email)
 
 
 @receiver(new_order)
@@ -63,17 +45,8 @@ def new_order_signal(user_id, **kwargs):
     """
     Отправка письма при изменении статуса заказа.
     """
-    # send an e-mail to the user
     user = User.objects.get(id=user_id)
-
-    msg = EmailMultiAlternatives(
-        # title:
-        f"Обновление статуса заказа",
-        # message:
-        'Заказ сформирован',
-        # from:
-        settings.EMAIL_HOST_USER,
-        # to:
-        [user.email]
-    )
-    msg.send()
+    title = 'Уведомление о смене статуса заказа'
+    message = 'Заказ сформирован.'
+    email = user.email
+    send_email.apply_async((title, message, email), countdown=5 * 60)
