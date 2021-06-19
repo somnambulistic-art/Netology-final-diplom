f90d69a7b7e65965052c6f7bdaeddc560d6ea550
Artyom <somnambulistic.traveller@gmail.com>
2021-06-19 12:43:24 +0300

    added SpectacularSwaggerView and SpectacularRedocView to urls.py in api_diplom_final dir

--- api_diplom_final/urls.py
+++ api_diplom_final/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('', include('usermanager.urls')),
     path('', include('shopmanager.urls')),
     path('', include('ordermanager.urls')),
